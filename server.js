41025583d2500819fec4f9b1390d099e609e4bcc
Boby Singh <126677943+Boby-Singh@users.noreply.github.com>
2026-07-12 18:07:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://servoracare.vercel.app/" // Replace after deploying to Vercel
+        "https://servoracare.vercel.app" // Replace after deploying to Vercel
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
