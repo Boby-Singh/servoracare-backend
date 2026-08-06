772bf89017565befe1c93e746033c14471502eb0
Boby Singh <126677943+Boby-Singh@users.noreply.github.com>
2026-08-07 00:00:10 +0530

    Force IPv4 for Gmail SMTP

--- config/mailer.js
+++ config/mailer.js
@@ -1,24 +1,53 @@
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
+
+// // const transporter = nodemailer.createTransport({
+// //     service: "gmail",
+// //     auth: {
+// //         user: process.env.EMAIL_USER,
+// //         pass: process.env.EMAIL_PASS,
+// //     },
+// // });
+
+// // module.exports = transporter;
+
+// // const nodemailer = require("nodemailer");
+
+// // const transporter = nodemailer.createTransport({
+// //     service: "gmail",
+// //     auth: {
+// //         user: process.env.EMAIL,
+// //         pass: process.env.EMAIL_PASSWORD,
+// //     },
+// // });
 
 // const transporter = nodemailer.createTransport({
-//     service: "gmail",
+
+//     host: "smtp.gmail.com",
+
+//     port: 465,
+
+//     secure: true,
+
 //     auth: {
-//         user: process.env.EMAIL_USER,
-//         pass: process.env.EMAIL_PASS,
-//     },
+//         user: process.env.EMAIL,
+//         pass: process.env.EMAIL_PASSWORD
+//     }
+
+// });
+
+// transporter.verify((error, success) => {
+
+//     if (error) {
+//         console.log("SMTP ERROR:", error);
+//     } else {
+//         console.log("SMTP READY");
+//     }
+
 // });
 
 // module.exports = transporter;
 
-// const nodemailer = require("nodemailer");
-
-// const transporter = nodemailer.createTransport({
-//     service: "gmail",
-//     auth: {
-//         user: process.env.EMAIL,
-//         pass: process.env.EMAIL_PASSWORD,
-//     },
-// });
+const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
 
@@ -28,12 +57,15 @@ const transporter = nodemailer.createTransport({
 
     secure: true,
 
+    family: 4, // Force IPv4
+
     auth: {
         user: process.env.EMAIL,
         pass: process.env.EMAIL_PASSWORD
     }
 
 });
+
 
 transporter.verify((error, success) => {
 
@@ -44,5 +76,6 @@ transporter.verify((error, success) => {
     }
 
 });
+
 
 module.exports = transporter;
