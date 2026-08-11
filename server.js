40c1b9d8c5d2d710a97c7b9e50f2596d66047ac0
Boby Singh <126677943+Boby-Singh@users.noreply.github.com>
2026-08-11 23:06:02 +0530

    Update navbar and website design

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express()
 
 app.use(cors({
     origin: [
+        "https://www.servoracare.in/",
         "https://servoracare.vercel.app" // Replace after deploying to Vercel
     ],
     credentials: true,
